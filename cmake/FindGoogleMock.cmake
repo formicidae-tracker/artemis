941d84ae215ac467ea618ed50f0345fc0859cd8e
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-03-22 13:12:00 +0100

    Adds googletest find package

---
+++ cmake/FindGoogleMock.cmake
@@ -0,0 +1,29 @@
+find_path(GMOCK_INCLUDE_DIR gmock/gmock.h
+	                        HINTS $ENV{GTEST_ROOT}/include
+	                              ${GTEST_ROOT}/include
+	                              )
+
+find_path(GMOCK_SRC_DIR src/gmock-all.cc
+	                    HINTS $ENV{GTEST_ROOT}/include
+	                          ${GTEST_ROOT}/include
+	                    PATHS /usr/src
+	                    PATH_SUFFIXES googletest/googlemock
+	                    )
+
+mark_as_advanced(GMOCK_INCLUDE_DIR GMOCK_SRC_DIR)
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GoogleMock DEFAULT_MSG GMOCK_SRC_DIR GMOCK_INCLUDE_DIR)
+
+if(GoogleMock_FOUND)
+	set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIR})
+
+	include(CMakeFindDependencyMacro)
+	find_dependency(GoogleTest)
+
+	add_library(gmock SHARED ${GMOCK_SRC_DIR}/src/gmock-all.cc)
+	target_link_libraries(gmock gtest)
+	target_include_directories(gmock PUBLIC ${GTEST_INCLUDE_DIRS} PRIVATE ${GMOCK_SRC_DIR} ${GMOCK_INCLUDE_DIR})
+	set(GMOCK_LIBRARIES gmock)
+
+endif(GoogleMock_FOUND)
---
+++ cmake/FindGoogleTest.cmake
@@ -0,0 +1,29 @@
+find_path(GTEST_INCLUDE_DIR gtest/gtest.h
+	                        HINTS $ENV{GTEST_ROOT}/include
+	                              ${GTEST_ROOT}/include
+	                              )
+
+find_path(GTEST_SRC_DIR src/gtest-all.cc
+	                    HINTS $ENV{GTEST_ROOT}/include
+	                          ${GTEST_ROOT}/include
+	                    PATHS /usr/src
+	                    PATH_SUFFIXES googletest/googletest
+	                    )
+
+mark_as_advanced(GTEST_INCLUDE_DIR GTEST_SRC_DIR)
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GoogleTest DEFAULT_MSG GTEST_SRC_DIR GTEST_INCLUDE_DIR)
+
+if(GoogleTest_FOUND)
+	set(GTEST_INCLUDE_DIRS ${GTEST_INCLUDE_DIR})
+
+	include(CMakeFindDependencyMacro)
+	find_dependency(Threads)
+
+	add_library(gtest SHARED ${GTEST_SRC_DIR}/src/gtest-all.cc)
+	target_link_libraries(gtest Threads::Threads)
+	target_include_directories(gtest PRIVATE ${GTEST_SRC_DIR} ${GTEST_INCLUDE_DIR})
+	set(GTEST_LIBRARIES gtest)
+
+endif(GoogleTest_FOUND)
