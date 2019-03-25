d3cd3f492d2186e77c872a36f8eff041c2efffb8
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-03-25 17:36:26 +0100

    Refactors FrameGrabber to gives Frame objects

--- src/FrameGrabber.cpp
+++ src/FrameGrabber.cpp
@@ -29,11 +29,41 @@ void FrameGrabber::Start() {
 FrameGrabber::~FrameGrabber() {
 }
 
-const FrameGrabber::BufferPtr & FrameGrabber::CurrentFrame() {
+const Frame::Ptr & FrameGrabber::CurrentFrame() {
 	return d_currentFrame;
 }
 
 void FrameGrabber::onNewBufferEvent(const Euresys::NewBufferData &data) {
-	d_currentFrame = std::make_shared<Euresys::ScopedBuffer>(*this,data);
+	d_currentFrame = std::make_shared<Frame>(*this,data);
 	d_manager->Signal(EventManager::FRAME_READY);
+}
+
+Frame::Frame(Euresys::EGrabber<Euresys::CallbackSingleThread> & grabber, const Euresys::NewBufferData & data)
+	: Euresys::ScopedBuffer(grabber,data)
+	, d_width(getInfo<size_t>(GenTL::BUFFER_INFO_WIDTH))
+	, d_height(getInfo<size_t>(GenTL::BUFFER_INFO_HEIGHT))
+	, d_timestamp(getInfo<uint64_t>(GenTL::BUFFER_INFO_TIMESTAMP))
+	, d_ID(getInfo<uint64_t>(GenTL::BUFFER_INFO_FRAMEID))
+	, d_mat(d_height,d_width,CV_8U,getInfo<void*>(GenTL::BUFFER_INFO_BASE)) {
+}
+
+Frame::~Frame() {}
+
+size_t Frame::Width() const {
+	return d_width;
+}
+size_t Frame::Height() const {
+	return d_height;
+}
+
+uint64_t Frame::Timestamp() const {
+	return d_timestamp;
+}
+
+uint64_t Frame::ID() const {
+	return d_ID;
+}
+
+const cv::Mat & Frame::ToCV() {
+	return d_mat;
 }
--- src/FrameGrabber.h
+++ src/FrameGrabber.h
@@ -5,6 +5,7 @@
 
 #include "EventManager.h"
 
+#include <opencv2/core.hpp>
 
 
 
@@ -15,6 +16,28 @@ struct CameraConfiguration {
 	int32_t  StrobeOffset;
 };
 
+
+class Frame : public Euresys::ScopedBuffer {
+public :
+	typedef std::shared_ptr<Frame> Ptr;
+
+	Frame(Euresys::EGrabber<Euresys::CallbackSingleThread> & grabber, const Euresys::NewBufferData &);
+	virtual ~Frame();
+
+
+	void * Data() const;
+	size_t Width() const;
+	size_t Height() const;
+	uint64_t Timestamp() const;
+	uint64_t ID() const;
+	const cv::Mat & ToCV();
+private :
+	size_t d_width,d_height;
+	uint64_t d_timestamp,d_ID;
+	cv::Mat d_mat;
+};
+
+
 class FrameGrabber : public Euresys::EGrabber<Euresys::CallbackSingleThread> {
 public :
 	typedef std::shared_ptr<Euresys::ScopedBuffer> BufferPtr;
@@ -23,16 +46,16 @@ public :
 	             const CameraConfiguration & cameraConfig,
 	             const EventManager::Ptr & eManager);
 
-	~FrameGrabber();
+	virtual ~FrameGrabber();
 
 	void Start();
 
-	const BufferPtr & CurrentFrame();
+	const Frame::Ptr & CurrentFrame();
 
 private:
 
 	virtual void onNewBufferEvent(const Euresys::NewBufferData &data);
 
 	EventManager::Ptr d_manager;
-	BufferPtr d_currentFrame;
+	Frame::Ptr d_currentFrame;
 };
